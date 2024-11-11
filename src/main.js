f5090c1f188cfe0f3a26483271edd67c9d6c7e63
jean <jeanpj12@gmail.com>
2024-11-11 19:34:16 -0300

    estruturação de header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 "use strict"
+
+// CSS
+import "./styles/global.css"
+import "./styles/header-logo.css"
+import "./styles/utility.css"
+import "./styles/main.css"
+import "./styles/inputs/index.css"
