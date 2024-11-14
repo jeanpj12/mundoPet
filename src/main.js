18708b43deb1f09169ec12eb32a8bec96fd4bffe
jean <jeanpj12@gmail.com>
2024-11-13 21:28:58 -0300

    Estruturação de pop up

--- src/main.js
+++ src/main.js
@@ -8,3 +8,14 @@ import "./styles/main.css"
 import "./styles/inputs/index.css"
 import "./styles/cards-schedules.css"
 import "./styles/button.css"
+import "./styles/popup/pop-up.css"
+
+const addNewScheduleButton = document.querySelector(".add-schedule")
+const popUpActive = document.querySelector(".active")
+const mainSelector = document.querySelector("main")
+
+if (popUpActive) {
+    addNewScheduleButton.style.display = "none"
+    mainSelector.style.filter = "blur(5px)"
+    document.body.style.overflow = "hidden";
+}
