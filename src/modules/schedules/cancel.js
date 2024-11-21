e7ac1f9655988c7ca26941f4e1b62bb1e00c09d0
jean <jeanpj12@gmail.com>
2024-11-21 17:13:27 -0300

    Exclusão de itens

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,1 +1,25 @@
-import 
+import { scheduleCancel } from "../../services/schedule-cancel";
+import { schedulesDay } from "./load";
+
+const periods = document.querySelectorAll(".schedules-area")
+
+periods.forEach((period) => {
+    period.addEventListener("click", async (e) => {
+        if(e.target.classList.contains("remove")) {
+            const item = e.target.closest("div")
+            const name = item.querySelector(".client-name").textContent.replace("/ ", "")
+            const {id} = item.dataset
+            
+            if(id) {
+                const isConfirm = confirm(
+                    `Deseja cancelar o agendamento de ${name}?`
+                );
+
+                if(isConfirm) {
+                    await scheduleCancel({id});
+                    schedulesDay()
+                }
+            }
+        }
+    })
+})
--- src/modules/schedules/show.js
+++ src/modules/schedules/show.js
@@ -1,57 +1,57 @@
 import dayjs from "dayjs";
 
-const schedulesMorning = document.getElementById("morning")
-const schedulesAfternoon = document.getElementById("afternoon")
-const schedulesNight = document.getElementById("night")
+const schedulesMorning = document.getElementById("morning");
+const schedulesAfternoon = document.getElementById("afternoon");
+const schedulesNight = document.getElementById("night");
 
+export function showSchedules({ dailySchedules }) {
+  schedulesMorning.innerHTML = "";
+  schedulesAfternoon.innerHTML = "";
+  schedulesNight.innerHTML = "";
 
-export function showSchedules({dailySchedules}) {
-    schedulesMorning.innerHTML = ""
-    schedulesAfternoon.innerHTML = ""
-    schedulesNight.innerHTML = ""
+  dailySchedules.forEach((schedule) => {
+    const scheduleItem = document.createElement("div");
+    scheduleItem.classList.add("schedule-item");
 
-    dailySchedules.forEach((schedule) => {
-        const scheduleItem = document.createElement("div")
-        scheduleItem.classList.add("schedule-item")
+    const clientWrapper = document.createElement("div");
 
-        const clientWrapper = document.createElement("div")
+    const scheduleHour = document.createElement("strong");
+    scheduleHour.classList.add("hour");
+    scheduleHour.textContent = dayjs(schedule.when).format("HH:mm");
 
-        const scheduleHour = document.createElement("strong")
-        scheduleHour.classList.add("hour")
-        scheduleHour.textContent= dayjs(schedule.when).format("HH:mm")
+    const petName = document.createElement("span");
+    petName.classList.add("pet-name");
+    petName.textContent = schedule.petName;
 
-        const petName = document.createElement("span")
-        petName.classList.add("pet-name")
-        petName.textContent = schedule.petName
+    const clientName = document.createElement("span");
+    clientName.classList.add("client-name");
+    clientName.textContent = `/ ${schedule.clientName}`;
 
-        const clientName = document.createElement("span")
-        clientName.classList.add("client-name")
-        clientName.textContent = `/ ${schedule.clientName}`
+    clientWrapper.append(scheduleHour, petName, clientName);
 
-        clientWrapper.append(scheduleHour, petName, clientName)
+    const service = document.createElement("span");
+    service.classList.add("service");
+    service.textContent = schedule.service;
 
-        const service = document.createElement("span")
-        service.classList.add("service")
-        service.textContent = schedule.service
+    const remove = document.createElement("span");
+    remove.classList.add("remove");
+    remove.textContent = "Remover agendamento";
 
-        const remove = document.createElement("span")
-        remove.classList.add("remove")
-        remove.textContent = "Remover agendamento"
+    scheduleItem.setAttribute("data-id", schedule.id);
+    scheduleItem.append(clientWrapper, service, remove);
 
-        scheduleItem.append(clientWrapper, service, remove)
+    const hour = dayjs(schedule.when).hour();
 
-        const hour = dayjs(schedule.when).hour()
+    if (hour <= 12) {
+      schedulesMorning.appendChild(scheduleItem);
+    }
 
-        if(hour <= 12) {
-            schedulesMorning.appendChild(scheduleItem)
-        }
+    if (hour > 12 && hour <= 18) {
+      schedulesAfternoon.appendChild(scheduleItem);
+    }
 
-        if (hour > 12 && hour <= 18) {
-            schedulesAfternoon.appendChild(scheduleItem)
-        }
-
-        if(hour > 18) {
-            schedulesNight.appendChild(scheduleItem)
-        }
-    })
+    if (hour > 18) {
+      schedulesNight.appendChild(scheduleItem);
+    }
+  });
 }
